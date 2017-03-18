72cd9f36a3703ea32ff60cbaa3468f3e51482f11
robertsbd <roberts.b.d@gmail.com>
2017-03-18 12:49:57 +0000

    using anonymous vars

--- prolog/day2.pl
+++ prolog/day2.pl
@@ -1,23 +1,32 @@
 % Reverse a list
 
 reverseList([], []).
-reverseList(X, [X]).
-reverseList(R, [X|Xs]) :- reverseList(T, Xs), append(T, [X], R).
+reverseList([R], R).
+reverseList([H|T], R) :- reverseList(T, T1), append(T1, [H], R).
 
 % Min of two numbers. Returns the case which is true. Can be seen by the first argument.
 minOfTwo(Y, X, Y) :- X >= Y.
 minOfTwo(X, X, Y) :- X < Y.
 
 % Min of a list, both of these work. I am going to use minOfList
-minOfList(X, [X]).
-minOfList([H|T], Min) :- minOfList(T, M1), (H<M1 -> Min is H ; Min is M1).
+min_in_list([Min], Min).
 
-minOfList2(X, [X]).
-minOfList2([H|T], Min) :- minOfList2(T, M1), minOfTwo(Min, H, M1).
+min_in_list([H, K|T], Min) :- 
+    H =< K,
+    min_in_list([H|T], Min).
+
+min_in_list([H, K|T], Min) :-
+    H > K,
+    min_in_list([K|T], Min).
 
 % Sort a list using minOfList
-gt(X, X, Y) :- X >= Y.
-gt(Y, X, Y) :- X < Y.
+sort_list([],[]).
+sort_list([S], S).
 
-sortList([S], [S]).
-sortList([H|T], S) :- sortList(T, S1), append(T, minOfList(T, Min), S).
+sort_list([H, K|T], S) :- 
+    H =< K,
+    sort_list([H, K|T], S).
+
+sort_list([H, K|T], Min) :-
+    H > K,
+    sort_list([K, H|T], S).
--- prolog/italian.pl
+++ prolog/italian.pl
@@ -6,12 +6,12 @@ word(pistola,  p,i,s,t,o,l,a).
 word(statale,  s,t,a,t,a,l,e).
 
 solve(V1, V2, V3, H1, H2, H3) :-
-    word(V1, V11, A, V13, B, V15, C, V17),
-    word(V2, V21, D, V23, E, V25, F, V27),
-    word(V3, V31, G, V33, H, V35, I, V37),    
-    word(H1, H11, A, H13, D, H15, G, H17),
-    word(H2, H21, B, H23, E, H25, H, H27),
-    word(H3, H31, C, H33, F, H35, I, H37),
+    word(V1, _, A, _, B, _, C, _),
+    word(V2, _, D, _, E, _, F, _),
+    word(V3, _, G, _, H, _, I, _),    
+    word(H1, _, A, _, D, _, G, _),
+    word(H2, _, B, _, E, _, H, _),
+    word(H3, _, C, _, F, _, I, _),
     V1 \= V2, V1 \= V3, V1 \= H1, V1 \= H2, V1 \= H3,
     V2 \= V3, V2 \= H1, V2 \= H2, V2 \= H3,
     V3 \= H1, V3 \= H2, V3 \= H3,
