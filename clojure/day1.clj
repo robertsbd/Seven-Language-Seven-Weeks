43ad6fd11b061437126503c655a1a5386c02e825
robertsbd <roberts.b.d@gmail.com>
2017-06-15 04:58:39 +0100

    @@

--- clojure/day1.clj
+++ clojure/day1.clj
@@ -3,6 +3,12 @@
 
 ;; Question 2
 
-(defn collection-type [col] ({(class []) :vector, (class '()) :emptylist, (class '(1)) :list, (class {}) :map} (class col)))
+(defn collection-type [col]
+  ({(class []) :vector, (class '()) :emptylist, (class '(1)) :list, (class {}) :map}
+   (class col)))
 
-
+;; ;; stuff from day 2 testing
+;; (defn size [v]
+;;   (if (empty? v)
+;;     0
+;;     (inc (size (rest v)))))
